0d29187d555b2a20b390e4efee6af645d0d76858
ANA CATARINA <251133647@dsn127m1242314.cfp127aluno.local>
2025-03-19 17:03:42 -0300

    Trapezio

---
+++ src/br/com/engsenai/calculadora_de_formas_geometricas/model/Trapezio.java
@@ -0,0 +1,53 @@
+package br.com.engsenai.calculadora_de_formas_geometricas.model;
+
+public class Trapezio {
+
+   private double baseMaior;	
+   private double baseMenor;
+   private double altura;
+   
+   public void setBaseMaior(double baseMaior) {
+	   this.baseMaior = baseMaior;
+   }
+	
+	public double getBaseMaior() {
+		return this.baseMaior;
+	}
+
+	public double getBaseMenor() {
+		return baseMenor;
+	}
+
+	public void setBaseMenor(double baseMenor) {
+		this.baseMenor = baseMenor;
+	}
+
+	public double getAltura() {
+		return altura;
+	}
+
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+	
+   private double CalcularArea() {
+	   double area = (baseMaior + baseMenor) * altura / 2;
+	   return area;
+   }
+	
+	public void mostrarDados() {
+		System.out.println("------------------");
+		System.out.println("T R A P E Z I O");
+		System.out.println("------------------");
+		System.out.println("Base Menor: " + baseMenor);
+		System.out.println("Base Maior: " + baseMaior);
+		System.out.println("Altura: " + altura);
+		System.out.println("Área: " + calcularArea());
+		System.out.println("------------------");
+		
+		
+		
+	}
+   
+   
+}
